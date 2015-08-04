3c40cb9bc6db20c3ec6d1302b027489b4e991908
whadla <will.hadla@gmail.com>
2015-08-04 09:49:35 -0600

    Test Commit
    Changed Value to 23

--- WebJob1/Program.cs
+++ WebJob1/Program.cs
@@ -18,6 +18,7 @@ namespace WebJob1
       // The following code will invoke a function called ManualTrigger and 
       // pass in data (value in this case) to the function
       host.Call(typeof(Functions).GetMethod("ManualTrigger"), new { value = 21 });
+      host.Call(typeof(Functions).GetMethod("ManualTrigger"), new { value = 23 });
     }
   }
 }
